ecd3e0005e9d5e51dca98f8c3b1be209c3168bcd
unknown <alexandrachiorean98@gmail.com>
2020-12-07 15:54:14 +0200

    Add inital notification files

---
+++ UrbanRider/app/src/main/java/Models/Notification.kt
@@ -0,0 +1,4 @@
+package Models
+
+class Notification (var id:Int, var title:String, var infos:String, var icon: Int) {
+}
---
+++ UrbanRider/app/src/main/java/com/piu/urbanrider/NotificationDriver.kt
@@ -0,0 +1,11 @@
+package com.piu.urbanrider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationDriver : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification_driver)
+    }
+}
