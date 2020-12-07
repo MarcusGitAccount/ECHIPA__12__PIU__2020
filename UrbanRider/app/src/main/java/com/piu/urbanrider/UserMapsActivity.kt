d92a9ae7b599265f9bbda17c54cce1ac26316261
Manuel Razvan Maior <7324236-manuel.maior@users.noreply.gitlab.com>
2020-12-07 17:25:32 +0200

    Simplified navigation drawer (needs items)

--- UrbanRider/app/src/main/java/com/piu/urbanrider/UserMapsActivity.kt
+++ UrbanRider/app/src/main/java/com/piu/urbanrider/UserMapsActivity.kt
@@ -2,6 +2,10 @@ package com.piu.urbanrider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
+import androidx.drawerlayout.widget.DrawerLayout
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,6 +17,9 @@ import com.google.android.gms.maps.model.MarkerOptions
 class UserMapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var navigationDrawer: DrawerLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +28,31 @@ class UserMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
+
+        this.navigationDrawer = findViewById(R.id.drawer_layout)
+
+        this.setupToolbar()
+        this.setupToggle()
+    }
+
+    private fun setupToolbar() {
+
+        this.toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    private fun setupToggle() {
+        this.toggle = ActionBarDrawerToggle(
+            this,
+            this.navigationDrawer,
+            this.toolbar,
+            R.string.open,
+            R.string.close
+        )
     }
 
     /**
