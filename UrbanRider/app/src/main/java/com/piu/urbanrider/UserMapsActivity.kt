11ca92f1829867ebf0ef0a728846f35703e88055
manuelmaior29 <62571222+manuelmaior29@users.noreply.github.com>
2020-12-08 15:56:07 +0200

    Manu common activities (#5)
    * Simplified navigation drawer (needs items)
    
    * Added recycler view for drawer options
    
    Co-authored-by: Manuel Razvan Maior <7324236-manuel.maior@users.noreply.gitlab.com>

--- UrbanRider/app/src/main/java/com/piu/urbanrider/UserMapsActivity.kt
+++ UrbanRider/app/src/main/java/com/piu/urbanrider/UserMapsActivity.kt
@@ -2,6 +2,12 @@ package com.piu.urbanrider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,10 +15,16 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.piu.urbanrider.adapters.DrawerOptionAdapter
+import com.piu.urbanrider.models.DrawerOptions
 
 class UserMapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var navigationDrawer: DrawerLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var drawerOptionAdapter: DrawerOptionAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +33,36 @@ class UserMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
+        this.navigationDrawer = findViewById(R.id.drawer_layout)
+        this.setupToolbar()
+        this.setupToggle()
+        this.setupDrawerOptions()
+    }
+
+    private fun setupDrawerOptions() {
+        val drawerOptionsRecyclerRef = findViewById<RecyclerView>(R.id.drawer_options_rv)
+        this.drawerOptionAdapter = DrawerOptionAdapter(this@UserMapsActivity, DrawerOptions().getDrawerOptions())
+        drawerOptionsRecyclerRef.adapter = this.drawerOptionAdapter
+        drawerOptionsRecyclerRef.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun setupToolbar() {
+        this.toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    private fun setupToggle() {
+        this.toggle = ActionBarDrawerToggle(
+            this,
+            this.navigationDrawer,
+            this.toolbar,
+            R.string.open,
+            R.string.close
+        )
     }
 
     /**
---
+++ UrbanRider/app/src/main/java/com/piu/urbanrider/models/DrawerOptions.kt
@@ -0,0 +1,69 @@
+package com.piu.urbanrider.models
+
+import com.piu.urbanrider.R
+
+class DrawerOptions {
+    private var drawerOptions = ArrayList<DrawerOption>()
+
+    init {
+        drawerOptions = getUserOptions()
+    }
+
+    fun getUserOptions(): ArrayList<DrawerOption> {
+        val list = ArrayList<DrawerOption>()
+        list.add(
+            DrawerOption(
+                1,
+                "Reviews",
+                R.drawable.ic_option_baseline_star_border_24
+            )
+        )
+        list.add(
+            DrawerOption(
+                1,
+                "Notifications",
+                R.drawable.ic_option_baseline_notifications_none_24
+            )
+        )
+        list.add(
+            DrawerOption(
+                1,
+                "Tickets",
+                R.drawable.ic_option_baseline_ticket_24
+            )
+        )
+        list.add(
+            DrawerOption(
+                1,
+                "Settings",
+                R.drawable.ic_option_baseline_settings_24
+            )
+        )
+        list.add(
+            DrawerOption(
+                1,
+                "Challenges",
+                R.drawable.ic_option_baseline_local_activity_24
+            )
+        )
+        list.add(
+            DrawerOption(
+                1,
+                "Share eco equipment",
+                R.drawable.ic_option_baseline_directions_bike_24
+            )
+        )
+        list.add(
+            DrawerOption(
+                1,
+                "Dashboard",
+                R.drawable.ic_option_baseline_dashboard_24
+            )
+        )
+        return list
+    }
+
+    fun getDrawerOptions(): ArrayList<DrawerOption> {
+        return this.drawerOptions
+    }
+}
