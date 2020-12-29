8113f1d5a11f2381ae00818769c57fe70fe80bf6
Manuel Razvan Maior <7324236-manuel.maior@users.noreply.gitlab.com>
2020-12-29 15:09:06 +0200

    Added layout + activity for transport results

---
+++ UrbanRider/app/src/main/java/com/piu/urbanrider/adapters/TransportResultAdapter.kt
@@ -0,0 +1,41 @@
+package com.piu.urbanrider.adapters
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.piu.urbanrider.models.TransportResult
+
+/*
+
+class TransportResultAdapter (private val context : Context, private val transportResults : ArrayList<TransportResult>) : RecyclerView.Adapter<> {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransportResultViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TransportResultViewHolder, position: Int) {
+    }
+
+    inner class TransportResultViewHolder(private var view : View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+        private lateinit var transportResultTitle : TextView
+        private lateinit var transportResultDescruption : TextView
+        private lateinit var transportResultImage : ImageView
+
+
+        override fun onClick(p0: View?) {
+            TODO("Not yet implemented")
+        }
+
+        fun bindData(data : TransportResult) {
+            this.data = data
+        }
+
+    }
+}
+ */
