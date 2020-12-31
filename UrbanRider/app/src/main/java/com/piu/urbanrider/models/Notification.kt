c477009f076112ef36d1fc645c52ac8b3a852a3f
Marcus Pop <mpop@monrtan.com>
2021-01-01 00:30:20 +0200

    Task 2

--- UrbanRider/app/src/main/java/com/piu/urbanrider/models/Notification.kt
+++ UrbanRider/app/src/main/java/com/piu/urbanrider/models/Notification.kt
@@ -1,4 +1,10 @@
 package com.piu.urbanrider.models
 
-class Notification (var id:Int, var title:String, var infos:String, var icon: Int) {
+class Notification (var id:Int, var title:String, var infos:String, var icon: Int, var type: NotificationType) {
+
+    enum class NotificationType {
+        NEW_RIDE_REQUEST,
+        RIDE_FINISHED,
+    }
+
 }
