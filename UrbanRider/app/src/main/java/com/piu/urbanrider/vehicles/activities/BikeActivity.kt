0e15c03bc2cde45fbf8055ab2d6e755e97d4c48d
unknown <alexandrachiorean98@gmail.com>
2021-01-05 19:45:26 +0200

    Add drawer and map to vehicles activities

--- UrbanRider/app/src/main/java/com/piu/urbanrider/vehicles/activities/BikeActivity.kt
+++ UrbanRider/app/src/main/java/com/piu/urbanrider/vehicles/activities/BikeActivity.kt
@@ -6,15 +6,43 @@ import android.view.View
 import android.widget.Button
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.piu.urbanrider.R
+import com.piu.urbanrider.adapters.DrawerOptionAdapter
+import com.piu.urbanrider.models.DrawerOptions
 import com.piu.urbanrider.models.vehicles.Bikes
 import com.piu.urbanrider.models.vehicles.RollerSkates
 
-class BikeActivity : AppCompatActivity() {
+class BikeActivity : AppCompatActivity(), OnMapReadyCallback {
     private var option: Int = 0
+    private lateinit var mMap: GoogleMap
+    private lateinit var navigationDrawer: DrawerLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var drawerOptionAdapter: DrawerOptionAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bike)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        this.navigationDrawer = findViewById(R.id.drawer_layout)
+        this.setupToolbar()
+        this.setupToggle()
+        this.setupDrawerOptions()
 
         val radioGroup_protection_equipment = findViewById<RadioGroup>(R.id.bike_rg)
         radioGroup_protection_equipment.setOnCheckedChangeListener(
@@ -26,7 +54,8 @@ class BikeActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.apply_button)
         button.setOnClickListener() {
             val result = Bikes.instance.getBikes(option)
-            System.out.println(result)
+            val transportResults = Bikes.instance.transform(result)
+            TODO("Connection to Transport Activity")
         }
     }
     fun radio_button_on_click(view: View)
@@ -43,5 +72,48 @@ class BikeActivity : AppCompatActivity() {
             r0.isChecked = true
             r1.isChecked = false
         }
+    }
+
+    private fun setupDrawerOptions() {
+        val drawerOptionsRecyclerRef = findViewById<RecyclerView>(R.id.drawer_options_rv)
+        this.drawerOptionAdapter = DrawerOptionAdapter(this@BikeActivity, DrawerOptions().getDrawerOptions())
+        drawerOptionsRecyclerRef.adapter = this.drawerOptionAdapter
+        drawerOptionsRecyclerRef.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun setupToolbar() {
+        this.toolbar = findViewById(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_drawer_toggle)
+    }
+
+    private fun setupToggle() {
+        this.toggle = ActionBarDrawerToggle(
+            this,
+            this.navigationDrawer,
+            this.toolbar,
+            R.string.open,
+            R.string.close
+        )
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
--- UrbanRider/app/src/main/java/com/piu/urbanrider/vehicles/activities/RollerActivity.kt
+++ UrbanRider/app/src/main/java/com/piu/urbanrider/vehicles/activities/RollerActivity.kt
@@ -9,15 +9,20 @@ import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.piu.urbanrider.R
 import com.piu.urbanrider.adapters.DrawerOptionAdapter
 import com.piu.urbanrider.models.DrawerOptions
 import com.piu.urbanrider.models.vehicles.RollerSkates
 
-class RollerActivity : AppCompatActivity() {
+class RollerActivity : AppCompatActivity(), OnMapReadyCallback {
     private var options: HashMap<String, String> = HashMap<String, String>()
     private lateinit var spinner: Spinner
+
     private lateinit var mMap: GoogleMap
     private lateinit var navigationDrawer: DrawerLayout
     private lateinit var toolbar: Toolbar
@@ -28,10 +33,10 @@ class RollerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_roller)
 
 
-        // this.navigationDrawer = findViewById(R.id.drawer_layout)
+        this.navigationDrawer = findViewById(R.id.drawer_layout)
         this.setupToolbar()
-        //this.setupToggle()
-        //this.setupDrawerOptions()
+        this.setupToggle()
+        this.setupDrawerOptions()
 
         populateDropBox()
         setOptions()
@@ -97,7 +102,8 @@ class RollerActivity : AppCompatActivity() {
         button.setOnClickListener() {
             options["shoes_number"] = spinner.selectedItem.toString()
             val result = RollerSkates.instance.getRollerSkaters(options)
-            System.out.println(result)
+            val transportResults = RollerSkates.instance.transform(result)
+            TODO("Connection to Transport Activity")
         }
     }
 
@@ -124,6 +130,24 @@ class RollerActivity : AppCompatActivity() {
             R.string.open,
             R.string.close
         )
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
 }
