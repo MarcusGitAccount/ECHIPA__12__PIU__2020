fbb79866fe0e72520d541bbfa3e822159afbba15
unknown <alexandrachiorean98@gmail.com>
2021-01-07 19:47:14 +0200

    Add roller manager layout. Modify bike manager layout

---
+++ UrbanRider/app/src/main/java/com/piu/urbanrider/vehicles/activities/RollerManageActivity.kt
@@ -0,0 +1,11 @@
+package com.piu.urbanrider.vehicles.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RollerManageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_roller_manage)
+    }
+}
---
+++ UrbanRider/app/src/main/java/com/piu/urbanrider/vehicles/activities/ScooterManageActivity.kt
@@ -0,0 +1,11 @@
+package com.piu.urbanrider.vehicles.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScooterManageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scooter_manage)
+    }
+}
