8d06349a8258b391dff2252d73e788fa86b50044
unknown <alexandrachiorean98@gmail.com>
2021-01-07 10:23:37 +0200

    Add navigation view and drawer to TransportResultsActivity

--- UrbanRider/app/src/main/java/com/piu/urbanrider/TransportResultsActivity.kt
+++ UrbanRider/app/src/main/java/com/piu/urbanrider/TransportResultsActivity.kt
@@ -2,13 +2,24 @@ package com.piu.urbanrider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.maps.GoogleMap
+import com.piu.urbanrider.adapters.DrawerOptionAdapter
 import com.piu.urbanrider.adapters.TransportResultAdapter
+import com.piu.urbanrider.models.DrawerOptions
 import com.piu.urbanrider.models.TransportResult
 import com.piu.urbanrider.models.TransportResults
 
 class TransportResultsActivity : AppCompatActivity() {
+
+    private lateinit var navigationDrawer: DrawerLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var drawerOptionAdapter: DrawerOptionAdapter
 
     var transportResultAdapter : TransportResultAdapter? = null
     private lateinit var transportResultRecyclerView : RecyclerView
@@ -16,6 +27,11 @@ class TransportResultsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transport_results)
+
+        this.navigationDrawer = findViewById(R.id.drawer_layout)
+        this.setupToolbar()
+        this.setupToggle()
+        this.setupDrawerOptions()
 
         transportResultRecyclerView = findViewById(R.id.recycler_view_transport_results)
 
@@ -25,6 +41,30 @@ class TransportResultsActivity : AppCompatActivity() {
         val linearLayoutManager = LinearLayoutManager(this)
         transportResultRecyclerView.layoutManager = linearLayoutManager
 
+    }
 
+    private fun setupDrawerOptions() {
+        val drawerOptionsRecyclerRef = findViewById<RecyclerView>(R.id.drawer_options_rv)
+        this.drawerOptionAdapter = DrawerOptionAdapter(this@TransportResultsActivity, DrawerOptions().getDrawerOptions())
+        drawerOptionsRecyclerRef.adapter = this.drawerOptionAdapter
+        drawerOptionsRecyclerRef.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun setupToolbar() {
+        this.toolbar = findViewById(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_drawer_toggle)
+    }
+
+    private fun setupToggle() {
+        this.toggle = ActionBarDrawerToggle(
+            this,
+            this.navigationDrawer,
+            this.toolbar,
+            R.string.open,
+            R.string.close
+        )
     }
 }
